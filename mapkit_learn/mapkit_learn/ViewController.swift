d007c2b31f94f77042920926bbdfd29660ac1958
intersignature <intersignature@hotmail.com>
2018-07-08 19:56:42 +0700

    track speed with simulator

--- mapkit_learn/mapkit_learn/ViewController.swift
+++ mapkit_learn/mapkit_learn/ViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mMapView: MKMapView!
+    @IBOutlet weak var speedLb: UILabel!
+    
+    var mLocationManager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        goToCenterLocation()
+        let myLocation: CLLocation = locations[0] as CLLocation
+        let latitude: CLLocationDegrees = myLocation.coordinate.latitude
+        let longtitude:CLLocationDegrees = myLocation.coordinate.longitude
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longtitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        mMapView.setRegion(region, animated: true)
+        mMapView.mapType = .hybrid
+        mMapView.showsCompass = true
+        mMapView.showsScale = true
+        
+        let speed = myLocation.speed * 3.6
+        speedLb.text = "\(speed) km/hr"
+    }
+    
+    func goToCenterLocation() {
+        if let locMan = mLocationManager.location {
+            let region = MKCoordinateRegionMakeWithDistance(locMan.coordinate, 400, 400)
+            mMapView.setRegion(region, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mLocationManager.delegate = self
+        mLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mMapView.delegate = self
+        
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse &&
+            CLLocationManager.authorizationStatus() != .authorizedAlways {
+            mLocationManager.requestWhenInUseAuthorization()
+        }
+        
+        mMapView.showsUserLocation = true
+        mLocationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
